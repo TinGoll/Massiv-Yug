abea07088e6ecd527c54def45f85999f00024846
Roman <roma.oliynyk83@gmail.com>
2023-11-12 21:20:11 +0300

    add test catalog in ignore

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -5,47 +5,7 @@ import { Box, Button, Typography } from "@mui/material";
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
-      <Box
-        sx={(theme) => ({
-          background: {
-            xs: "#476b35",
-            md: "#b95135",
-            lg: "#3893ac",
-            laptop: "#e1bc39",
-          },
-          minHeight: "100vh",
-        })}
-      >
-        <Button type="button" variant="contained" color="primary">
-          Кнопка
-        </Button>
-        <Button type="button" variant="contained" color="secondary">
-          Кнопка
-        </Button>
-        <Button type="button" variant="contained" color="info">
-          Кнопка
-        </Button>
-        <Button type="button" variant="contained" color="error">
-          Кнопка
-        </Button>
-        <Button type="button" variant="contained" color="inherit">
-          Кнопка
-        </Button>
-        <Button type="button" variant="contained" color="success">
-          Кнопка
-        </Button>
-        <Button type="button" variant="contained" color="warning">
-          Кнопка
-        </Button>
-        <Box>
-          <Typography variant="poster">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-            perferendis possimus in! Sit rem dolore ducimus, officiis nostrum
-            deleniti quaerat architecto. Repudiandae, fugiat facere. Pariatur
-            adipisci ea a possimus sunt!{" "}
-          </Typography>
-        </Box>
-      </Box>
+      Главная
     </main>
   );
 };
