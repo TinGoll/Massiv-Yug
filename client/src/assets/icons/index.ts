bdc2adfaf2ac414c425d1c17722048c9bbab0f3c
Roman <roma.oliynyk83@gmail.com>
2023-11-13 23:53:12 +0300

    add icon components

---
+++ client/src/assets/icons/index.ts
@@ -0,0 +1,41 @@
+import Approve from "./approve";
+import ApproveFile from "./approve-file-icon";
+import Bookmark from "./bookmark-icon";
+import Chip from "./chip-icon";
+import Compressi from "./compress-icon";
+import Guarantee from "./guarantee-icon";
+import Lamp from "./lamp-icon";
+import Lifebuoy from "./lifebuoy-icon";
+import Like from "./like-icon";
+import Location from "./location-icon";
+import Logo from "./logo-icon";
+import LowerRightArrow from "./lower-right-arrow-icon";
+import MobileMenu from "./mobile-menu-icon";
+import PDF from "./pdf-icon";
+import Phone from "./phone-icon";
+import Ruler from "./ruler-icon";
+import Star from "./star-icon";
+import Transform from "./transform-icon";
+import WoodTexture from "./wood-texture-icon";
+
+export {
+  ApproveFile,
+  Approve,
+  Bookmark,
+  Chip,
+  Compressi,
+  Guarantee,
+  Lamp,
+  Lifebuoy,
+  Like,
+  Location,
+  Logo,
+  LowerRightArrow,
+  MobileMenu,
+  PDF,
+  Phone,
+  Ruler,
+  Star,
+  Transform,
+  WoodTexture,
+};
