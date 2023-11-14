4cfb919633fc2b78af9964e3068d317c7b0e1783
Roman <roma.oliynyk83@gmail.com>
2023-11-14 23:05:51 +0300

    setting theme and fix svg icon

--- client/src/theme/theme.ts
+++ client/src/theme/theme.ts
@@ -1,16 +1,23 @@
 import { createTheme, ThemeOptions } from "@mui/material/styles";
 
+interface ColorPelete {
+  primary: string;
+  secondary: string;
+  background: string;
+  tertiary: string;
+}
+
+interface Colors {
+  light: ColorPelete;
+  dark: ColorPelete;
+}
 
 declare module "@mui/material/styles" {
   interface Theme {
-    status: {
-      danger: string;
-    };
+    colors: Colors;
   }
   interface ThemeOptions {
-    status?: {
-      danger?: string;
-    };
+    colors: Colors;
   }
   interface BreakpointOverrides {
     mobile: true;
@@ -21,17 +28,34 @@ declare module "@mui/material/styles" {
 
   interface TypographyVariants {
     poster: React.CSSProperties;
+    title: React.CSSProperties;
+    text: React.CSSProperties;
+    subtitle: React.CSSProperties;
+    bigNumber: React.CSSProperties;
+    blockHeader: React.CSSProperties;
+    blockNumber: React.CSSProperties;
   }
 
-  // allow configuration using `createTheme`
   interface TypographyVariantsOptions {
     poster?: React.CSSProperties;
+    title?: React.CSSProperties;
+    text?: React.CSSProperties;
+    subtitle?: React.CSSProperties;
+    bigNumber?: React.CSSProperties;
+    blockHeader?: React.CSSProperties;
+    blockNumber?: React.CSSProperties;
   }
 }
 
 declare module "@mui/material/Typography" {
   interface TypographyPropsVariantOverrides {
     poster: true;
+    title: true;
+    text: true;
+    subtitle: true;
+    bigNumber: true;
+    blockHeader: true;
+    blockNumber: true;
   }
 }
 
@@ -42,24 +66,61 @@ export const themeOptions: ThemeOptions = {
       color: "#333333",
       fontFamily: "Orchidea Pro",
     },
-  },
-  status: {
-    danger: "#E9345B",
+    title: {
+      fontSize: "55px",
+      fontWeight: 600,
+      lineHeight: "64.63px",
+      color: "#000000",
+      fontFamily: "Orchidea Pro",
+    },
+    text: {
+      fontSize: "16px",
+      fontWeight: 400,
+      lineHeight: "24.4px",
+      color: "#000000",
+    },
+    subtitle: {
+      fontSize: "25px",
+      fontWeight: 600,
+      lineHeight: "33.25px",
+      color: "#000000",
+      fontFamily: "Orchidea Pro",
+    },
+    bigNumber: {
+      fontSize: "104px",
+      fontWeight: 300,
+      lineHeight: "105.04px",
+      color: "#000000",
+      fontFamily: "Orchidea Pro",
+    },
+    blockHeader: {
+      fontSize: "16px",
+      fontWeight: 500,
+      lineHeight: "21.28px",
+      color: "#000000",
+      textTransform: "uppercase",
+    },
+    blockNumber: {
+      fontSize: "27px",
+      fontWeight: 600,
+      lineHeight: "35.91px",
+      color: "#FFC099",
+    },
   },
   palette: {
     // mode: "light",
-    primary: {
-      main: "#333333",
-    },
-    secondary: {
-      main: "#E9345B",
-    },
-    error: {
-      main: "#c71668",
-    },
-    warning: {
-      main: "#DC9D00",
-    },
+    // primary: {
+    //   main: "#333333",
+    // },
+    // secondary: {
+    //   main: "#E9345B",
+    // },
+    // error: {
+    //   main: "#c71668",
+    // },
+    // warning: {
+    //   main: "#DC9D00",
+    // },
   },
   breakpoints: {
     values: {
@@ -80,6 +141,20 @@ export const themeOptions: ThemeOptions = {
       "@media (min-width:600px)": {
         minHeight: 138,
       },
+    },
+  },
+  colors: {
+    dark: {
+      primary: "#E9345B",
+      secondary: "#333333",
+      background: "#FFFFFF",
+      tertiary: "#FFC099",
+    },
+    light: {
+      primary: "#E9345B",
+      secondary: "#333333",
+      background: "#FFFFFF",
+      tertiary: "#FFC099",
     },
   },
 };
