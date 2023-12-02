c620503b5a5e937c8c8aec5cad02fce66445362e
Roman <roma.oliynyk83@gmail.com>
2023-12-02 21:29:51 +0300

    add nginx.conf and docker-compose

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -33,4 +33,4 @@ const About = () => {
 
 export default About;
 
-export const Head: HeadFC = () => <title>Home Page</title>;
+export const Head: HeadFC = () => <title>Массив юг - о компании.</title>;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
-import { Container } from "@mui/material";
 import { Banner } from "../features/banner";
 import Layout from "../layouts/layout";
 import CarouselBlock from "../features/carousel-block";
@@ -17,126 +16,6 @@ const IndexPage: React.FC<PageProps> = () => {
         <AdvantagesBlock />
         <OrderProcessBlock />
         <PartnerBlock />
-        <div>
-          <Container maxWidth="desktop">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nobis
-            debitis sit necessitatibus quis voluptatibus at, sint eligendi
-            suscipit ratione cumque totam accusamus? Voluptatum ex odio error
-            quod mollitia ea at, doloremque neque asperiores a voluptatibus,
-            deleniti pariatur minima tempora sit possimus quae excepturi.
-            Dignissimos nulla quis minima asperiores mollitia ratione. Quae
-            pariatur laudantium voluptates tenetur debitis nemo eaque
-            praesentium nam at, quisquam deleniti autem aliquid a repellat. Quos
-            rerum eligendi sed perspiciatis iusto possimus ut error aperiam
-            omnis earum veniam cupiditate dolorem architecto ipsum odio laborum
-            quasi adipisci voluptates in, excepturi laboriosam nemo nobis?
-            Placeat nemo totam dolorem fuga delectus, optio at inventore velit
-            facilis necessitatibus est recusandae corrupti non provident
-            voluptatum perspiciatis eius molestiae tenetur, similique sapiente.
-            A amet praesentium alias iste repellat ipsa labore asperiores
-            officiis non. Est sunt iste necessitatibus recusandae, delectus
-            magnam aut tempora eius alias modi saepe, impedit nisi praesentium
-            dolorem veritatis debitis nostrum. Eveniet labore quia
-            necessitatibus sunt quod illum, eos a alias aspernatur sed, totam
-            minus adipisci ipsam voluptatem! Minus atque dolorum eveniet
-            doloribus ipsa, modi dignissimos. Possimus iusto rerum cupiditate!
-            Corporis facilis beatae, exercitationem aspernatur deserunt eum,
-            consequatur odio provident dolorum cumque adipisci sequi mollitia
-            nemo esse culpa vel in autem atque temporibus est reiciendis
-            voluptate id. Harum minus consequuntur iusto consectetur enim dicta
-            hic officia, quas praesentium ab possimus impedit dolor saepe non?
-            Nemo omnis, ipsa rem officia quasi aliquam quo similique? Est veniam
-            architecto eum reiciendis dolore, neque totam dolorum consectetur
-            sint aperiam pariatur quisquam officia magnam tempora esse vero sed
-            iure nulla porro dolor. Voluptatum aut consequuntur illo sunt quo
-            itaque eaque! Inventore eum sapiente, itaque veniam laudantium id
-            quisquam modi quia est numquam placeat eos, iure maiores doloremque
-            adipisci harum dolorum molestias quae ipsum asperiores? Molestiae
-            sunt quod reprehenderit eius. Amet reiciendis adipisci illum maxime
-            eaque harum autem fuga vitae? Doloribus deleniti labore facere neque
-            beatae consequuntur mollitia earum atque quos quasi molestias
-            incidunt eligendi repudiandae laboriosam, aliquid asperiores enim
-            ratione alias natus dignissimos rerum. Fuga necessitatibus,
-            voluptatibus atque dignissimos eligendi officia, aspernatur quidem
-            reiciendis odio consectetur deserunt quisquam veniam beatae?
-            Perferendis rerum in dolorem molestiae ipsa, cum tempora, quas
-            similique nobis culpa deserunt velit impedit totam porro
-            praesentium? Nostrum quidem, ea accusamus consequuntur aliquam
-            cupiditate praesentium maiores cumque at, vel, culpa odio.
-            Voluptatem at, doloribus consequatur quod tenetur assumenda omnis
-            nostrum tempore maxime. Officiis atque odit pariatur voluptate
-            reprehenderit dignissimos ad autem expedita nobis esse, porro culpa?
-            Suscipit ex sequi veniam id, odit consequuntur natus quisquam
-            deserunt ut repellendus esse, consequatur, cupiditate magnam dicta
-            in possimus fuga dignissimos distinctio. Nam, suscipit quas
-            praesentium eius aliquam quam voluptatibus corporis, molestiae eum
-            cumque doloribus ad nesciunt tempore tenetur, eaque maxime? Quam
-            voluptate architecto vel, quae soluta beatae esse. Totam debitis
-            quae itaque sapiente quia, perferendis voluptas reiciendis
-            accusantium neque qui adipisci veritatis odio facere tempora quas
-            dolores! Qui accusantium cumque, rerum quasi repudiandae quam
-            impedit saepe possimus blanditiis maiores inventore ducimus. Rem,
-            repellendus ipsam sed blanditiis magni cupiditate aliquam ipsa omnis
-            laudantium dolorum voluptate vel, odit obcaecati quasi illum.
-            Perferendis, incidunt! Fugit facilis error distinctio magni quasi
-            molestias dignissimos maxime dolorum, necessitatibus doloribus
-            voluptatem repudiandae. Laborum eaque, officia velit natus iste est
-            ad dicta hic harum rem illum nobis impedit quasi esse asperiores
-            sequi distinctio vel alias, repellendus eum voluptatem voluptas
-            labore quos! Suscipit quas repudiandae inventore id voluptatem
-            maxime quam, eius hic est voluptate cum blanditiis earum tenetur
-            placeat vel quaerat officiis ullam unde libero molestiae tempora
-            odit accusamus. Aspernatur quisquam ipsa in magni incidunt harum
-            impedit quidem quae laboriosam perspiciatis, iure amet nisi odio
-            illo, voluptates officiis, fugiat delectus porro. Ab amet provident
-            repudiandae accusamus. Saepe iure expedita aperiam? Magni quos
-            corrupti nostrum quisquam amet atque. Vel deleniti consectetur alias
-            et, placeat officiis officia nesciunt necessitatibus explicabo quis
-            facilis, labore ipsa debitis vero provident numquam ab illum
-            suscipit, pariatur aspernatur cum dolorum commodi distinctio. Quod
-            ducimus quaerat, aut iste dignissimos, sapiente debitis possimus
-            corrupti dolorum aliquid commodi doloremque illum libero reiciendis
-            exercitationem labore magni quas numquam odit dolor error eligendi
-            vero beatae nam. Est quo nihil rem ipsa distinctio assumenda
-            delectus eum adipisci optio, itaque ratione numquam. Provident
-            soluta ab sit iusto sapiente, quae sed possimus libero aliquid ipsum
-            esse perspiciatis repellat magni. Officia consequatur, vero
-            voluptatibus error fugit facere dolore fugiat quam vitae ipsa? Sit
-            recusandae natus eius enim! Enim nihil praesentium esse dolores
-            commodi ab ad, quas nemo, cupiditate labore facilis dolor velit
-            magnam vitae, impedit voluptatibus! Consectetur consequatur, ratione
-            eaque ipsam accusamus illum autem, tempore corrupti, voluptates
-            cupiditate vitae. Numquam omnis et commodi id nihil, quaerat aliquid
-            quas explicabo cupiditate esse ab eum ex aut ea doloremque unde,
-            modi illum harum reiciendis? Nostrum corporis, et inventore ipsa
-            modi libero quos accusamus dolore culpa vel sed aspernatur
-            voluptatum, autem rem numquam molestias excepturi. Saepe voluptate
-            enim qui itaque voluptatibus? Perferendis voluptatum assumenda
-            officia ipsum cum quaerat sequi illo facere vero quam quia at, sunt
-            optio impedit. Facilis excepturi unde mollitia perspiciatis
-            voluptate, sequi odit deleniti iusto, aut accusamus nesciunt
-            adipisci harum provident vitae temporibus labore quam consequatur
-            animi, minus laboriosam? Laborum veritatis, quisquam totam ad magnam
-            deleniti recusandae voluptas pariatur, quam delectus quis aut
-            corporis reprehenderit molestiae voluptates nemo nostrum ullam
-            incidunt consequatur odio? Consequuntur, eligendi. Necessitatibus et
-            porro id maxime aperiam ab nemo iusto architecto aspernatur
-            distinctio magni, illum quo quasi sequi animi laborum eveniet
-            adipisci? Dolor corrupti eius ea fugiat ullam, nesciunt ad? Saepe
-            officiis doloremque iste nisi minus officia eum maxime velit. Aut,
-            cum quod, totam sed omnis officia sit voluptatum, rem explicabo
-            eveniet aliquam obcaecati praesentium quaerat enim iste laudantium.
-            Perspiciatis non blanditiis consequatur alias, reiciendis ex saepe
-            architecto quod soluta deleniti maxime corrupti ipsam fuga quis
-            similique, aut sed repellendus ullam esse animi. Harum, modi est
-            rerum dicta fuga aspernatur necessitatibus? Natus cum autem beatae
-            corrupti sit veritatis quas amet ipsa eius harum tenetur optio enim
-            dolore, placeat a, fuga in deleniti, iure quia eum facere dolorum
-            nesciunt. Aperiam esse sunt praesentium odit a in laudantium culpa
-            est commodi, neque autem consectetur quibusdam fugiat beatae
-            cupiditate, modi quae mollitia.
-          </Container>
-        </div>
       </main>
     </Layout>
   );
@@ -144,4 +23,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>;
+export const Head: HeadFC = () => <title>Массив юг - главная</title>;
