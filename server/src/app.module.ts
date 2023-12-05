c2a32363e216b4cb81a1f57405797094482382de
Roman <roma.oliynyk83@gmail.com>
2023-12-05 20:49:01 +0300

    setting server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        const staticFolder = config.get('STATIC_FILES_PATH');
+        const rootPath = join(__dirname, '..', '..', staticFolder);
+        return [{ rootPath, exclude: ['/api/(.*)'] }];
+      },
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `../.env`,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
